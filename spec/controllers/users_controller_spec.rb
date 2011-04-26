88540dec2244e614aaa1d455570593f2f6449770
Josef Roshan <josefroshan@gmail.com>
2011-04-26 18:26:17 +0530

    Finished layout and routes

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+		
+		it "should have the right title" do
+        get 'home'
+        response.should have_selector("title",
+                    :content => @base_title + " | Sign up")
+    end
+  end
+	
+	
+
+end
